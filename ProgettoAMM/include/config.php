102c1757fc0906cafc5c04a497894c036dcf4e57
amm <amm@amm-VirtualBox>
2015-03-19 12:52:07 +0100

    config db 2

--- ProgettoAMM/include/config.php
+++ ProgettoAMM/include/config.php
@@ -4,7 +4,7 @@
 $host="127.0.0.1";        // MySQL Host ip
 $user="silvanoEnrico";       // MysQL DB User
 $pass="criceto6923";      // MySQL DB Pass
-$dbname="amm15";     // MySQL DB Name
+$dbname="amm15_";     // MySQL DB Name
 
 mysql_connect($host,$user,$pass) or die('Connection Error.');
 mysql_select_db($dbname) or die('Not found database.');
