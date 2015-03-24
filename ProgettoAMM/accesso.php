f66e1bb9b731e7cfa3cb90ec6114fba904c13b93
amm <amm@amm-VirtualBox>
2015-03-24 23:24:27 +0100

    img dimention

--- ProgettoAMM/accesso.php
+++ ProgettoAMM/accesso.php
@@ -13,7 +13,7 @@
     <div class="tabellapiccola">
         <div class="rigatr">
         <div class="colonnatd25"><div class="border">  <?php include 'template/templateMENU.php';?></div></div>
-            <div class="colonnatd75"><div class="border"><img src="images/logo2.png" alt="logo" title="logo website">  </div></div>
+            <div class="colonnatd75"><div class="border"><img src="images/logo2.png" width="611" height="248" alt="logo" title="logo website">  </div></div>
         
  </div>
     </div>
