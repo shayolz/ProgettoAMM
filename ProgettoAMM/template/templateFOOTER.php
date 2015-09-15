b4e06a53a16a4678f0d9132e4b547674e76c14a1
shayolz <albenr@hotmail.it>
2015-09-15 10:21:10 +0200

    Update templateFOOTER.php

--- ProgettoAMM/template/templateFOOTER.php
+++ ProgettoAMM/template/templateFOOTER.php
@@ -26,11 +26,7 @@
                             </td>
                             <td>
                                 <p>
-                                    <a href="http://jigsaw.w3.org/css-validator/check/referer">
-                                        <img style="border:0;width:88px;height:31px"
-                                             src="http://jigsaw.w3.org/css-validator/images/vcss"
-                                             alt="CSS Valido!" />
-                                    </a>
+                                    <!-- validatore HTML -->
                                 </p>
                             </td>
                         </tr>
